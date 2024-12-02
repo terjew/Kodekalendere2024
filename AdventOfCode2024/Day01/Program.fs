e9acf8c52b009f0babf19fc143433157c3326876
Terje Wiesener <terje.wiesener@lyse.no>
2024-12-02 16:19:48 +0100

    Solve day 1

--- AdventOfCode2024/Day01/Program.fs
+++ AdventOfCode2024/Day01/Program.fs
@@ -1,2 +1,57 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let getNumbers pattern line =
+    Regex.Matches(line, pattern) 
+        |> Seq.map (fun m -> m.Groups[1].Value |> int)
+
+let parse =
+    getNumbers "([0-9]+)"
+
+let getLeftAndRightLists seq =
+    let left = Seq.map Seq.head seq
+    let right = Seq.map Seq.last seq
+    (left,right)
+
+let sortBothLists (left,right) =
+    let sortedLeft =  left |> Seq.sort
+    let sortedRight = right |> Seq.sort
+    (sortedLeft, sortedRight)
+
+let distance (l,r) =
+   r - l |> abs
+
+let pairwiseDistance (left,right) =
+    let pairs = Seq.zip left right
+    pairs |> Seq.map distance
+
+let getOccurenceMap seq =
+    seq |> Seq.countBy id |> Map
+
+let getCount item dict =
+    if dict |> Map.containsKey item then dict |> Map.find item else 0
+
+let getSimilarityScore item dict =
+    item * getCount item dict
+
+let calculateSimilarities (left, right) =
+    let lookup = getOccurenceMap right 
+    left |> Seq.map (fun l -> getSimilarityScore l lookup)
+
+let input = File.ReadLines("input.txt")
+let sample = File.ReadLines("sample.txt")
+
+input
+|> Seq.map parse
+|> getLeftAndRightLists
+|> sortBothLists
+|> pairwiseDistance
+|> Seq.sum
+|> printfn "Part 1: %A"
+
+input
+|> Seq.map parse
+|> getLeftAndRightLists
+|> calculateSimilarities
+|> Seq.sum
+|> printfn "Part 2: %A"
