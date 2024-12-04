b6119b7c1046eea24188ef5af289a6f433f5a619
Terje Wiesener <terje.wiesener@lyse.no>
2024-12-04 23:05:10 +0100

    Solve day3

---
+++ AdventOfCode2024/Day03/Day03.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let parseMuls line =
+    Regex.Matches(line, @"mul\((\d{1,3}),(\d{1,3})\)") 
+    |> Seq.map (fun m -> (int m.Groups[1].Value, int m.Groups[2].Value))
+
+let mul (a,b) =
+    a * b
+
+let parseActiveSections (text:string) =
+    Regex.Matches("do()"+text+"don't()", @"(?:do\(\))(.*?)(?:don't\(\))", RegexOptions.Singleline) 
+    |> Seq.map (fun m -> m.Groups[1].Value)
+
+File.ReadLines("input.txt")
+|> Seq.collect parseMuls
+|> Seq.sumBy mul
+|> printfn "Part 1: %A"
+
+File.ReadAllText("input.txt")
+|> parseActiveSections
+|> Seq.collect parseMuls
+|> Seq.sumBy mul
+|> printfn "Part 2: %A"
+
