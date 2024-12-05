6b7650dd07c5d01217167dc0df530fbf6ad03b45
Terje Wiesener <terje.wiesener@lyse.no>
2024-12-05 01:06:44 +0100

    Day04 part 2

--- AdventOfCode2024/Day04/Day04.fs
+++ AdventOfCode2024/Day04/Day04.fs
@@ -6,7 +6,7 @@ let checkPos pos matrix expected =
     | false -> false
     | true -> Matrix.get matrix pos |> (fun v -> v = expected)
 
-let checkDirection pos matrix direction = 
+let checkDirectionXMAS pos matrix direction = 
     let offset = Coordinate.offsetWith pos direction
     let check (pos, expected) = checkPos pos matrix expected
 
@@ -19,11 +19,31 @@ let checkDirection pos matrix direction =
     if isXmas then 1 else 0
    
 let checkForXmas pos matrix =
-    Direction.cardinalAndOrdinal |> Seq.sumBy (checkDirection pos matrix)
+    Direction.cardinalAndOrdinal |> Seq.sumBy (checkDirectionXMAS pos matrix)
 
+let checkDirectionMAS pos matrix direction = 
+    let offset = Coordinate.offsetWith pos direction
+    let check (pos, expected) = checkPos pos matrix expected
+
+    let isXmas = seq {(-1,'M'); (1,'S')}
+                |> Seq.map (fun (a,c) -> (offset a, c))
+                |> Seq.map check 
+                |> Seq.contains false 
+                |> not
+
+    if isXmas then 1 else 0
+   
+let checkForMas pos matrix =
+    let count = Direction.ordinal 
+                |> Seq.sumBy (checkDirectionMAS pos matrix)
+    if count = 2 then 1 else 0
 
 let matrix = File.ReadLines("input.txt") |> Matrix.fromStrings 
-let startingPositions = matrix |> Matrix.findAll 'X'
 
-startingPositions |> Seq.sumBy (fun p -> checkForXmas p matrix)
+matrix |> Matrix.findAll 'X'
+|> Seq.sumBy (fun p -> checkForXmas p matrix)
+|> printfn "Part 1: %A"
+
+matrix |> Matrix.findAll 'A'
+|> Seq.sumBy (fun p -> checkForMas p matrix)
 |> printfn "Part 1: %A"
